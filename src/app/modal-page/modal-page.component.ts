fbc5ba5d0e6a3a8825e0afc72fb05e98b1daf0cb
DESKTOP-T9TKHKU\Zhenya <ekovtunov13@gmail.com>
2018-06-13 01:06:08 +0300

    End Slider and Video

--- src/app/modal-page/modal-page.component.ts
+++ src/app/modal-page/modal-page.component.ts
@@ -2,33 +2,34 @@ import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { HomePageComponent } from '../home-page/home-page.component';
 import { Angular2TokenService } from 'angular2-token';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-modal-page',
   templateUrl: './modal-page.component.html',
   styleUrls: ['./modal-page.component.scss']
 })
 export class ModalPageComponent implements OnInit {
-  constructor(
+
+  
+  videoUrl: SafeResourceUrl;
+  dangerousVideoUrl: string;
+  counter:number;
+  
+  constructor(private sanitizer: DomSanitizer,
     public dialogRef: MatDialogRef<HomePageComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ){ }
+  ) {
+    this.updateVideoUrl();
+   }
 
   ngOnInit() {
-    let counter=Object.keys(this.data.item.gallery_images).length;
-    console.log("counter",counter);
-    console.log("data",this.data);
-
-    let shos=this.data.item.long_description;
-    for(let i=0;i<shos.length;i++)
-    {
-      if(shos[i]==0)console.log("ok");
-    }
+     this.counter = Object.keys(this.data.item.gallery_images).length;
+  }
   
+  updateVideoUrl() {
+    this.dangerousVideoUrl = this.data.item.videoUrl;
+    this.videoUrl =
+        this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
   }
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
- 
+  
 }
